9b72fa53eae7b5546436864b5419c06c8f9d7eea
0639Quercus <90751130+0639Quercus@users.noreply.github.com>
2022-07-24 14:01:47 +0700

    Create Main.java

---
+++ hw2/Main.java
@@ -0,0 +1,54 @@
+package hw2;
+
+public class Main {
+        public static void main(String[] args) {
+          /*
+           DynamicArray var = new DynamicArray(2);
+           var.printStructure();
+           var.pushBack(5);
+           var.pushBack(6);
+           var.printStructure();
+           var.pushBack(7);
+           var.printStructure();
+           var.pushBack(8);
+           var.printStructure();
+           var.pushBack(9);
+           var.printStructure();
+          /*
+          
+          /*
+         DynamicArray var = new DynamicArray(3);
+         var.printStructure();
+         var.popBack();
+         var.pushBack(11);
+         var.pushBack(12);
+         var.popBack();
+         var.printStructure();
+          */
+          
+          /*
+         DynamicArray var = new DynamicArray(2);
+         for (int i=0; i<10; i++){
+         var.pushBack(i*i + 1);
+         }
+         var.printStructure();
+         System.out.println(var.get(3));
+         var.set(3, 555);
+         System.out.println(var.get(3));
+         var.remove(3);
+         var.printStructure();
+          */
+          
+          /*
+         DynamicArray var = new DynamicArray(2);
+         for (int i=0; i<10; i++){
+         var.pushBack(i*i + 1);
+         }
+         var.printStructure();
+         System.out.println(var.get(10));
+         var.set(10, 555);
+         var.remove(10);
+         var.printStructure();
+          */
+    }
+}
