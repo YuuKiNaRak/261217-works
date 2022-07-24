295d93514bb725f4529b7ca207c9f2d6edfdb7f9
0639Quercus <90751130+0639Quercus@users.noreply.github.com>
2022-07-24 13:45:30 +0700

    Update DoublyLinkedList.java

--- hw3/DoublyLinkedList.java
+++ hw3/DoublyLinkedList.java
@@ -13,13 +13,13 @@ public class DoublyLinkedList {
         if (isEmpty()){
             System.out.println("ERROR");
         }else{
-            if(tail == head){
+            if(tail == head){ //check if the list has a single node
                 tail = null;
                 head = null;
                 return;
             }
-            tail = tail.previous; //point tail to the node previous to tail
-
+            tail = tail.previous; 
+            tail.next = null; //set next of the last node to null since it was pointing to previous tail
         }
     }
     
@@ -27,13 +27,13 @@ public class DoublyLinkedList {
         if (isEmpty()){
             System.out.println("ERROR");
         }else{
-            if(tail == head){
-                System.out.println("HELLO");
-                tail = null;
+            if(tail == head){ //check if the list has a single node
+                tail = null; 
                 head = null;
                 return;
             }
-            head = head.next; //point head to the node next to head
+            head = head.next;
+            head.previous = null; //set previous of the first node to null since it was pointing to previous head
         }
     }
     
@@ -42,7 +42,7 @@ public class DoublyLinkedList {
             System.out.println("ERROR");
             return new Node("Empty List!");
         } else {
-            return head;
+            return head; //return the first node
         }
     }
     
@@ -51,29 +51,29 @@ public class DoublyLinkedList {
             System.out.println("ERROR");
             return new Node("Empty List!");
         } else {
-            return tail;
+            return tail; //return the last node
         }
     }
     
     public void pushFront(Node node){
         if (isEmpty()){
-            head = node;
+            head = node; //If the list was empty, both head and tail will point toward the input node.
             tail = node;
         }else{
-            head.previous = node; //assign previous of head to node
-            node.next = head; //assign next of node to head
-            head = node; //reassign head to the new node
+            head.previous = node; //assign previous of head to node     node <-  HEAD <->...
+            node.next = head; //assign next of node to head             node <-> HEAD <->...
+            head = node; //reassign head to the new node                HEAD <-> previous_head <->...
         }
     }
     
     public void pushBack(Node node) {
         if (isEmpty()) {
-            head = node;
+            head = node; //If the list was empty, both head and tail will point toward the input node.
             tail = node;
         } else {
-            tail.next = node; //assign next of tail to node
-            node.previous = tail; //assign previous of node to tail
-            tail = node;  // reassign tail to the new node
+            tail.next = node; //assign next of tail to node             ... <-> TAIL  -> node
+            node.previous = tail; //assign previous of node to tail     ... <-> TAIL <-> node
+            tail = node;  //reassign tail to the new node              ... <-> previous_tail <-> TAIL
         }
     }
 
@@ -81,83 +81,89 @@ public class DoublyLinkedList {
         if (isEmpty()){
             return new Node("Empty List!");
         } else {
-            Node current = head; //Assign new node "current" pointing it to head
-            while(current != null){ //loop until current is null
-                if(current.student_id == id) //check if 
-                    return current;
+            Node current = head;
+            while(current != null){ //iterate each node in the list
+                if(current.student_id == id) //check if a node's student_id is equal to id
+                    return current; //if yes, return the node and exit the function
                 current = current.next;
             }
-
+            //in case there are no any node that satisfies the condition        
             return new Node("Student Not Found!");
         }
     }
     
     public Node eraseNode(int id){
         if (isEmpty()){
+            System.out.println("ERROR");
             return new Node("Empty List!");
         } else {
-            Node current = head; //Assign new node "current" pointing it to head
-            while(current != null){ //loop until current is null
-                if(current.student_id == id){ //check if 
-                    if(current != head)
+            Node current = head;
+            while(current != null){ //iterate each node in the list
+                if(current.student_id == id){ //if found the node which its student_id is equal to id
+                    if(current != head) //if the node isn't head
                         current.previous.next = current.next;
                     else{
                         head = head.next;
-                        head.previous = null;
+                        head.previous = null; //set previous of the new_head to null since it was pointing to previous_head
                     }
-                    if(current != tail)
+                    if(current != tail) //if the node isn't tail
                         current.next.previous = current.previous;
                     else{
                         tail = tail.previous;
-                        tail.next = null;
+                        tail.next = null; //set next of the new_tail to null since it was pointing to previous_tail
                     }
 
-                    return current;
+                    return current; //return the erased node
                 }
                 current = current.next;
             }
+            //in case there are no any node that satisfies the condition       
             return new Node("Student Not Found!");
         }
     }
     
     public void addNodeAfter(Node node1, Node node2){
-        if(node1 == tail)
+        if(node1 == tail)   //check if node1 is the tail
             tail = node2;
-        else
-            node2.next = node1.next;
-        node2.previous = node1;
-        node1.next = node2;
+        else{//if not                           <----------------->
+            node2.next = node1.next;        //node1   node2  -> node3  
+            node2.next.previous = node2;    //node1   node2 <-> node3
+        }
+        node2.previous = node1; //node1 <-  node2
+        node1.next = node2;     //node1 <-> node2
     }
     
     public void addNodeBefore(Node node1, Node node2){
-        if(node1 == head)
+        if(node1 == head)   //check if node1 is the head
             head = node2;
-        else
-            node2.previous = node1.previous;
-        node2.next = node1;
-        node1.previous = node2;
+        else{//if not                               <----------------->
+            node2.previous = node1.previous;    //node3 <-  node2   node1
+            node2.previous.next = node2;        //node3 <-> node2   node1
+        }
+        node2.next = node1;     //node2  -> node1
+        node1.previous = node2; //node2 <-> node1
     }
     
     public boolean isEmpty(){
-        return (head == null); // return true if head is null;
+        return (head == null); //return true if head is null;
     }
     public void merge(DoublyLinkedList list){
-        this.tail.next = list.head;
+        this.tail.next = list.head;         //connects 2 lists together
         list.head.previous = this.tail;
-        this.tail = list.tail;
+        this.tail = list.tail;  //reassign main-tail to point toward the inputlist-tail
     }
     
     public void printStructure(){
         Node current=head;
         System.out.print(listName + ": head <-> ");
-        while(current!=null){
+        while(current!=null){ //iterate each node in the list
             System.out.print("{" + current.student_id + "} <-> ");
             current = current.next;
         }
         System.out.println("tail");
     }
     
-    // This may be useful for you for implementing printStructure()
+    //This may be useful for you for implementing printStructure()
     public void printStructureBackward(){ 
         Node current=tail;
         System.out.print(listName + ": tail <-> ");
@@ -172,15 +178,15 @@ public class DoublyLinkedList {
         if (isEmpty()) {
             return new Node("Empty List!");
         } else {
-            Node maxGpa = head;
+            Node maxgpaNode = head;
             Node current = head.next;
-            while(current != null){
-                if(current.gpa >= maxGpa.gpa)
-                    maxGpa = current;
+            while(current != null){ //iterate each nodes in the list
+                if(current.gpa >= maxgpaNode.gpa)   //if considering max-gpa is greater than or equal to saved max-gpa
+                    maxgpaNode = current;           //reassign max-gpa to the pointing node
                 current = current.next;
             }
 
-            return maxGpa;
+            return maxgpaNode; //return max-gpa
         }
     }
 }
