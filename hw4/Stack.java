f2bbf7a048a4de8a28af51e2588e17352b7ed1bd
Denisska1231 <jedthaphon@gmail.com>
2022-07-31 14:04:16 +0700

    stack finished

--- hw4/Stack.java
+++ hw4/Stack.java
@@ -6,17 +6,17 @@ public class Stack implements List{
     
     public void push(Node node){
         if (head == null){
-            head = node;
+            head = node; //add node to stack(empty stack)
         }else{
             node.next = head;
-            head = node;
+            head = node; //add node to the top of stack (not empty stack)
         }
     }
     
     public void pop(){
         // Fix this function
         if (head != null){
-            head = head.next;
+            head = head.next; // pop out the node at the top of stack
         }else{
             System.out.println("Error: Stack Underflow");
         }
@@ -24,7 +24,7 @@ public class Stack implements List{
     
     public Node top(){
         
-        return head;
+        return head; //return node at the top of stack to caller
     }
     
 }
