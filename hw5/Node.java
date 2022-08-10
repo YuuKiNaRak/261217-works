92beaed52c2c3eb5f7b8750599532e3ccfc271ab
0639Quercus <90751130+0639Quercus@users.noreply.github.com>
2022-08-10 17:10:18 +0700

    Added Comments

--- hw5/Node.java
+++ hw5/Node.java
@@ -1,13 +1,13 @@
 package hw5;
 
-public class Node extends BTreePrinter { // Fix this line
+public class Node extends BTreePrinter {
 
     Node left;
     Node right;
     int data;
 
     public Node(int data) {
-        this.data = data;
+        this.data = data; //assign data
     }
 
     public void printTree() {
@@ -17,12 +17,12 @@ public class Node extends BTreePrinter { // Fix this line
     public void printBFT() {
         Queue q = new Queue(50);
         System.out.print("BFT node sequence [ ");
-        q.enqueue( this );
-        Node node;
+        q.enqueue( this ); //enqueue itself into the Queue
+        Node node; //declare a Node
         while ( !q.isEmpty() ){
             node = q.dequeue();
-            System.out.print(node.data + " ");
-            if (node.left != null)
+            System.out.print(node.data + " "); //print
+            if (node.left != null) //enqueue its children
                 q.enqueue(node.left);
             if (node.right != null)
                 q.enqueue( node.right);
@@ -33,12 +33,12 @@ public class Node extends BTreePrinter { // Fix this line
     public void printDFT() { // PreOrder
         Stack s = new Stack(50);
         System.out.print("DFT node sequence [ ");
-        s.push( this );
-        Node node;
+        s.push( this ); //push itself into the Stack
+        Node node; //declare a Node
         while ( !s.isEmpty() ){
             node= s.pop();
-            System.out.print(node.data + " "); 
-            if (node.right != null)
+            System.out.print(node.data + " "); //print
+            if (node.right != null) //push its children
                 s.push(node.right);
             if (node.left != null)
                 s.push( node.left);
