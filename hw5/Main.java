e0ebce0b06e56bf04dd450f85b8e5c3c9db445c0
Denisska1231 <90751218+Denisska1231@users.noreply.github.com>
2022-08-10 15:20:53 +0700

    constructTree2 error fixed

--- hw5/Main.java
+++ hw5/Main.java
@@ -22,9 +22,9 @@ public class Main {
         root.left = new Node(2);
         root.left.left = new Node(4);
         root.left.right = new Node(5);
-        root.left.right.left = new Node(7);
-        root.left.right.left.right = new Node(10);
+       root.left.right.left = new Node(7);
         root.left.right.right = new Node(8);
+        root.left.right.right.right = new Node(10);
         
         root.right = new Node(3);
         root.right.right = new Node(6);
