71a3d96ccc2961e199d4622a23efa689b783127c
Yuuki <90751280+YuuKiNaRak@users.noreply.github.com>
2022-08-10 14:59:28 +0700

    up

--- hw5/Node.java
+++ hw5/Node.java
@@ -1,29 +1,49 @@
 package hw5;
 
-public class Node {
+public class Node extends BTreePrinter { // Fix this line
 
     Node left;
     Node right;
     int data;
 
     public Node(int data) {
-
+        this.data = data;
     }
 
     public void printTree() {
-        // uncomment the following line and fix the errors
-        // super.printTree(this);
+        super.printTree(this);
     }
 
     public void printBFT() {
         Queue q = new Queue(50);
         System.out.print("BFT node sequence [ ");
+        q.enqueue( this );
+        Node node;
+        while ( !q.isEmpty() ){
+            node = q.dequeue();
+            System.out.print(node.data + " ");
+            if (node.left != null)
+                q.enqueue(node.left);
+            if (node.right != null)
+                q.enqueue( node.right);
+        }
         System.out.println("]");
     }
 
     public void printDFT() { // PreOrder
         Stack s = new Stack(50);
         System.out.print("DFT node sequence [ ");
+        s.push( this );
+        Node node;
+        while ( !s.isEmpty() ){
+            node= s.pop();
+            System.out.print(node.data + " "); 
+            if (node.right != null)
+                s.push(node.right);
+            if (node.left != null)
+                s.push( node.left);
+        }
         System.out.println("]");
     }
 }
+
